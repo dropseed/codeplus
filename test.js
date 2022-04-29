68529fcfd49aa0336336b584f496c4540aedea37
Dave Gaeddert <dave.gaeddert@gmail.com>
2022-04-29 14:14:10 -0500

    Add tests and jest

---
+++ test.js
@@ -0,0 +1,28 @@
+import parse from "./parse";
+
+test("parse filename", () => {
+  const parsed = parse("# filename.js\nfunction foo() {}");
+  expect(parsed).toEqual({
+    filename: "filename.js",
+    displayName: "",
+    groupName: "",
+  });
+});
+
+test("parse filename and display", () => {
+  const parsed = parse("# filename.js (GitHub with spaces)\nfunction foo() {}");
+  expect(parsed).toEqual({
+    filename: "filename.js",
+    displayName: "GitHub with spaces",
+    groupName: "",
+  });
+});
+
+test("parse filename, display, group", () => {
+  const parsed = parse("# filename.js (GitHub with spaces) - group example\nfunction foo() {}");
+  expect(parsed).toEqual({
+    filename: "filename.js",
+    displayName: "GitHub with spaces",
+    groupName: "group example",
+  });
+});
